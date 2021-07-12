786b7e681fbe47aa150712d80d20a9e2e9eeebed
井手 俊文 <knocknote.ide@gmail.com>
2021-07-12 16:55:26 +0900

    create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+<title>My BBS</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="container">
+            <h1>My BBS</h1>
+            <ul>
+                <li>Title</li>
+                <li>Title</li>
+                <li>Title</li>
+            </ul>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+</html>
