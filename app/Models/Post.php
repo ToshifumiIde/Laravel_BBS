e9d5f3abbabb7273398de9a0dec6259ff9ca1a5e
井手 俊文 <knocknote.ide@gmail.com>
2021-07-13 13:32:24 +0900

    create migration

---
+++ app/Models/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+//Laravelの仕様でPostモデルは -> posts テーブルに紐づく
+class Post extends Model
+{
+    use HasFactory;
+}
