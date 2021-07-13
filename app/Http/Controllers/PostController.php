10837309f9ef751fb14e57428a8a14ec3abd66d0
井手 俊文 <knocknote.ide@gmail.com>
2021-07-13 18:20:48 +0900

    get sql data by using PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,27 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //privatePropertyに$postsを設定
-    private $posts = [
-        "TitleA",
-        "TitleB",
-        "TitleC",
-    ];
-
+    // private $posts = [
+    //     "TitleA",
+    //     "TitleB",
+    //     "TitleC",
+    // ];//mysqlからpostsデータを取得するため、ここの記述は不要
     //トップにアクセスするメソッド
     public function index()
     {
-    return view('index')
-        ->with(["posts" => $this->posts]);
+        //$posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+
+        return view('index')
+            ->with(["posts" => $posts]);
     }
     //各ページの詳細ページにアクセスするメソッド
     public function show($id)
     {
         //postsディレクトリのshow.blade.phpを呼び出すには、posts.showと書く必要がある。
+        $post = Post::findOrFail($id);
+        
         return view('posts.show')
-            ->with(['post' => $this->posts[$id]]);
+            ->with(['post' => $post]);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,13 +2,13 @@
     {{-- lauyout.blade.phpで設定した$titleに値を格納していく。 --}}
     {{-- 方法は、<X-slot name="title"></x-solt>で囲い、name属性に変数名を格納する。--}}
     <x-slot name="title">
-        {{$post}} - My BBS
+        {{$post->title}} - My BBS
     </x-slot>
     <div class="back-link">
         &laquo; <a href="{{route('posts.index')}}">back</a>
             {{-- 名前をつけたroutingを呼び出す場合、{{route()}}を使用--}}
             {{-- 今回はposts.indexという名前のroutingを呼ぶため{{route('posts.index')}} を呼ぶ--}}
     </div>
-    <h1>{{$post}}</h1>
-    <h2>こんにちは</h2>
+    <h1>{{$post->title}}</h1>
+    <p>{{$post->body}}</p>
 </x-layout>
